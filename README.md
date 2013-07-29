c109689db356bdfcec93ad6c0a8fe7357f1a2052
Brian Gonzalez <brian.gonzalez@geni.com>
2013-07-29 12:28:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## jquery.arbitrary-anchor.js
+
+This plugin allows the page to scroll to any arbitrary object on the page.
---
+++ jquery.arbitrary-anchor.js
@@ -0,0 +1,50 @@
+;(function(window, $){
+
+  var $window, $document, $body;
+  var animationLength = 750;
+
+  // Document ready?
+  // Well then do the magic.
+  $(document).ready(function(){
+
+    // Grab our elements.
+    $window   = $(window);
+    $document = $(this);
+    $body     = $document.find('body');
+
+    // Scroll to the anchor on initial page load.
+    scrollToHash();
+    $window.on('hashchange', function(){ scrollToHash(); })
+
+  })
+
+  function scrollToHash(){
+    var hash    = window.location.hash.substring(1);
+
+    // What are valid values for the id attribute.
+    // http://stackoverflow.com/questions/70579/what-are-valid-values-for-the-id-attribute-in-html
+    if ( hash.charAt(0).search(/[A-Za-z]/) > -1 )
+      var $actualID         = $( "#" + hash);
+
+    var $actualAnchor     = $('a[name="'+ hash +'"]');
+
+    // Let the browser handle the default types of anchors.
+    if ( ($actualAnchor && $actualAnchor.length > 0) || ($actualID && $actualID.length > 0) )
+      return;
+
+    // Store the arbitrary 
+    var $arbitraryAnchor  = $(hash).first();
+    if ( $arbitraryAnchor && $arbitraryAnchor.length > 0 ) {
+      $el = $arbitraryAnchor;      
+    } else {
+      return;
+    }
+
+    // Scroll to $el
+    if ( $el.length > 0 ) {
+      var top = $el.offset().top;
+      $body.animate({ scrollTop: top }, 750)
+    }
+  }
+
+})(window, jQuery)
